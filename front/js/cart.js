11c217f73879e4a2af9e4fb61db8781bd4bfc104
Cedric Pintado <pintadocedric@gmail.com>
2022-10-27 10:15:12 +0200

    supression

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,15 +1,20 @@
 //Permet de récupérer les produits du localstorage
-const products = [];
 const panier = JSON.parse(localStorage.getItem('panier'));
+let totalPrice;
+let totalQty;
 
 //Appelle la fonction displayData
 getData();
 
-let totalPrice = 0;
-let totalQty = 0;
-
 //Permet d'afficher les produits du panier
 async function getData() {
+
+    const cartItems = document.getElementById('cart__items');
+    cartItems.innerHTML = '';
+
+    totalPrice = 0;
+    totalQty = 0;
+
     for (let i = 0; i < panier.length; i++) {
 //Récupérer les info produits individuellement
     const productLs = panier[i];
@@ -20,7 +25,7 @@ async function getData() {
     
 //Réunis les deux en une seule grace à la syntaxe spread operator... qui permet d'étendre toutes les données de l'objet et de les insérer dans un nouvel objet    
     const product =  { ...productLs, ...productInfo };
-    console.log(product); 
+   
 
 //Afficher les données 
     displayData(product);  
@@ -30,16 +35,22 @@ async function getData() {
 
 //Fonction pour calculer la quantité totale
     displayTotalQuantity(product);
-
-
+   
     }
 }
+
+ //Fonction pour modifier la quantité 
+    modifyQuantity();
+
+
+ //Fonction pour supprimer dezs produits   
+    deleteProduct();
 
 function displayData(product) {
     const cartItems = document.getElementById('cart__items');
 
     cartItems.innerHTML += `
-        <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+        <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
             <div class="cart__item__img">
                 <img src="${product.imageUrl}" alt=${product.altTxt}>
             </div>
@@ -60,6 +71,7 @@ function displayData(product) {
             </div>
         </div>
     </article>`;
+
 }
 
 function displayTotalPrice(product){
@@ -76,6 +88,64 @@ function displayTotalQuantity(product){
     totalQty += product.quantity;
 
     cartQty.innerHTML = totalQty;
+}
+
+function modifyQuantity(){
+    const inputs = document.querySelectorAll('.itemQuantity');
+
+    for(let i = 0; i < inputs.length; i++){
+        const input = inputs[i];
+        input.addEventListener('change', (event) => {
+            const userValue = parseInt(event.target.value);
+
+            if (userValue < 1){
+                alert('Veuillez renseigner une quantité supérieure à 0');
+                return;
+            }
+
+            const article = input.closest('article');
+            const id = article.dataset.id;
+            const color = article.dataset.color;
+
+            const currentProduct = panier.find(
+                (product) => product.id === id && product.color === color);
+
+            currentProduct.quantity = userValue;
+
+                localStorage.setItem("panier", JSON.stringify(panier));
+                
+                getData ();
+        });
+    }
+}
+
+
+function deleteProduct(){
+    const inputs = document.querySelectorAll('.itemQuantity');
+
+    for(let i = 0; i < inputs.length; i++){
+        const input = inputs[i];
+        input.addEventListener('change', (event) => {
+            const userValue = parseInt(event.target.value);
+
+            if (userValue < 1){
+                alert('Veuillez renseigner une quantité supérieure à 0');
+                return;
+            }
+
+            const article = input.closest('article');
+            const id = article.dataset.id;
+            const color = article.dataset.color;
+
+            const currentProduct = panier.splice((product) => product.id === id && product.color === color);
+
+            currentProduct.quantity = userValue;
+
+                localStorage.setItem("panier", JSON.stringify(panier));
+                
+                getData ();
+        });
+    }
 }
 
 //sélection du html
